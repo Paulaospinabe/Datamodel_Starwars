7f3d2f4f345473b2e716dc125c7811a5b8170fce
Paulaospinabe <ospinapaula6.1@gmail.com>
2022-04-01 01:01:20 +0000

    Datamodel Star Wars

--- src/models.py
+++ src/models.py
@@ -8,23 +8,64 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class StarWars(Base):
+    __tablename__ = 'StarWars'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    ID = Column(primary_key=True)
+    Characters = Column(String(250), nullable=False)
+    Planets = Column(String(250), nullable=False)
+    Starships = Column(String(250), nullable=False)
+    Favourites = Column(Integer)
+    
 
-class Address(Base):
-    __tablename__ = 'address'
+class Characters(Base):
+    __tablename__ = 'Characters'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    ID = Column(primary_key=True)
+    Name = Column(String(250), ForeignKey('StarWars.ID'), ForeignKey('Favourites.Characters_Name'))
+    Description = Column(String(250), nullable=False)
+    Created = Column(String(250), nullable=False)
+    Gender = Column(String(250), nullable=False)
+    Eye_Color = Column(String(250), nullable=False)
+    Hair_Color = Column(String(250), nullable=False)
+    
+
+class Planets(Base):
+    __tablename__ = 'Planets'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    ID = Column(primary_key=True)
+    Name = Column(String(250), ForeignKey('StarWars.ID'), ForeignKey('Favourites.Planets_Name'))
+    Description = Column(String(250), nullable=False)
+    Created = Column(String(250), nullable=False)
+    Climate = Column(String(250), nullable=False)
+    Terrain = Column(String(250), nullable=False)
+   
+
+class Starships(Base):
+    __tablename__ = 'Starships'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    ID = Column(primary_key=True)
+    Model = Column(String(250), ForeignKey('StarWars.ID'), ForeignKey('Favourites.Starships_Name'))
+    Description = Column(String(250), nullable=False)
+    Starship_Class = Column(String(250), nullable=False)
+    Manufacturer = Column(String(250), nullable=False)
+    Max_Atmosphering_Speed = Column(String(250), nullable=False)
+   
+
+class Favourites(Base):
+    __tablename__ = 'Favourites'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    ID = Column(primary_key=True)
+    Characters_Name = Column(String(250), nullable=False)
+    Planets_Name = Column(String(250), nullable=False)
+    Starships_Name = Column(String(250), nullable=False)
+   
+    
 
     def to_dict(self):
         return {}
